f3ae6419ebc078ee7d8545e4dc576b9243923e10
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2022-10-27 19:05:09 -0300

    Botão de Limpar centralizado e enviar apertando o Enter

--- classes.py
+++ classes.py
@@ -29,17 +29,18 @@ class Chat:
 
     def createWidgets(self):
         #Declarando os widgets do chat
-        self.txt_chat = Text(self.canva, border = 1)
-        self.txt_field = Entry(self.canva, width = 85, border = 1, bg = 'white')
+        self.txt_chat = Text(self.canva, border = 1, state=DISABLED)
+        self.txt_field = Entry(self.canva, width = 85,border = 1, bg = 'white')
         self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)
+        self.window.bind('<Return>', self.send)
         self.clean_button = Button(self.canva, text = "Limpar", padx = 40, command = self.clean)
         
         #Posicionando os Widgets
         self.txt_chat.config(background = "#363636", foreground='white')
         self.txt_chat.grid(column = 0, row = 0, columnspan = 3)
-        self.txt_field.grid(column = 0, row = 1, columnspan = 2)
+        self.txt_field.grid(column = 0, row = 1, columnspan = 2, rowspan=3)
         self.send_button.grid(column = 2, row = 1)
-        self.clean_button.grid(column = 3, row = 1)
+        self.clean_button.grid(column = 2, row = 2)
     
     def clean(self):
         #Limpando tela de mensagens
@@ -53,8 +54,10 @@ class Chat:
         #Impedindo o envio de mensagens vazias
         if text_aux != "":
             current_time = "<" + datetime.now().strftime('%d/%m/%Y %H:%M') + "h" + "> "
+            self.txt_chat.configure(state=NORMAL)
             self.txt_chat.insert(END, current_time + self.name + ": " + self.text + '\n')
             self.txt_field.delete(0, END)
+            self.txt_chat.configure(state=DISABLED)
 
             #Enviando a mensagem para o outro usuário
             try:
@@ -64,8 +67,10 @@ class Chat:
 
     def receive(self, text):
         self.text_received = text + "\n"
+        self.txt_chat.configure(state=NORMAL)
         current_time = "<" + datetime.now().strftime('%d/%m/%Y %H:%M') + "h" + "> "
         self.txt_chat.insert(END, current_time + self.text_received)
+        self.txt_chat.configure(state=DISABLED)
 
     def connect_server(self):
         #Abrindo conexão com o server
@@ -125,7 +130,8 @@ class Login:
         self.txt_port = Entry(self.canva, width = 85, border = 1, bg = 'white')   
 
         #Declarando o Botão
-        self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)        
+        self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)     
+        self.window.bind('<Return>', self.send)   
         
         #Posicionando os Widgets
         self.label1.grid(column = 0, row = 1, columnspan = 1)
