ac3c4f9394ae3bb57c1256e8340488669d33464f
Felipe Moraes <felipe3902@gmail.com>
2022-10-28 23:46:42 -0300

    Add UDP connection

--- classes.py
+++ classes.py
@@ -17,8 +17,9 @@ class Chat:
 
     def __init__(self, largura, altura):
         #Informações do usuário
-        self.name, self.port = Login(1024, 768).start()
-        self.server_port = 50000
+        self.name, self.port_tcp = Login(1024, 768).start()
+        self.server_port_tcp = 50000
+        self.server_port_udp = ('localhost', 50001)
 
         #Declarando as informações da janela
         self.window = Tk()
@@ -36,6 +37,10 @@ class Chat:
         #Thread para receber mensagens
         thread_get_message = threading.Thread(target=self.get_message, args=(), daemon=True)
         thread_get_message.start()
+
+        #Thread UDP
+        thread_get_message_udp = threading.Thread(target=self.get_message_udp, args=(), daemon=True)
+        thread_get_message_udp.start()
 
     def createWidgets(self):
         #Declarando os widgets do chat
@@ -69,7 +74,15 @@ class Chat:
         global play_button
         global play_video_button
 
-        self.window.filename = filedialog.askopenfilename()
+        #ENVIA UMA MENSAGEM UDP AO CLICAR EM UPLOAD
+        #self.addr -> ENDEREÇO UDP DO OUTRO CLIENTE
+        try:
+            self.socket_udp.sendto("Teste".encode('utf-8'), self.addr)
+        except:
+            pass
+
+        self.window.file = filedialog.askopenfile()
+        self.window.filename = self.window.file.name
         nome_arquivo = self.window.filename.split('.')
         tipo_arquivo = nome_arquivo[len(nome_arquivo) - 1]
 
@@ -97,11 +110,10 @@ class Chat:
             self.txt_chat.window_create(END, window=Button(self.window, text="Play Video", padx = 40, command=self.playVideo))
             self.txt_chat.insert(END, '\n')
 
-
     def clean(self):
         #Limpando tela de mensagens
-        self.txt_chat.delete("1.0", "end") 
-    
+        self.txt_chat.delete("1.0", "end")
+
     def send(self, event = None):
         self.text = self.txt_field.get()
         text_aux = self.text.replace(' ', '')
@@ -126,14 +138,19 @@ class Chat:
     def connect_server(self):
         #Abrindo conexão com o server
         self.client_server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        self.client_server.bind(('localhost', self.port))
-        self.client_server.connect(('localhost', self.server_port))
+        self.client_server.bind(('localhost', self.port_tcp))
+        self.client_server.connect(('localhost', self.server_port_tcp))
         self.client_server.send(self.name.encode('utf-8'))
+        time.sleep(0.03)
+        self.socket_udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.socket_udp.sendto("OI".encode('utf-8'), self.server_port_udp)
 
         #RECEBE DO SERVER OS DADOS DO OUTRO CLIENTE
         self.name_p2p = self.client_server.recv(1024).decode('utf-8')
         self.host_p2p = self.client_server.recv(1024).decode('utf-8')
         self.port_p2p = int(self.client_server.recv(1024).decode('utf-8'))
+        addr_1 = int(self.client_server.recv(1024).decode('utf-8'))
+        self.addr = ('localhost', addr_1)
 
         #Fechando conexão com o server
         self.client_server.close()
@@ -141,9 +158,9 @@ class Chat:
     def connect_p2p(self):
         #Abrindo uma conexão P2P com o outro cliente
         self.client_p2p = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        self.client_p2p.bind(('localhost', self.port))
+        self.client_p2p.bind(('localhost', self.port_tcp))
         self.client_p2p.connect((self.host_p2p, self.port_p2p))
-
+                
     def get_message(self):
         while True:
             try:
@@ -153,7 +170,25 @@ class Chat:
                 self.receive(self.name_p2p + ": " + self.msg_received)
             except:
                 pass
+    
+    def get_message_udp(self):
+        while True:
+            msg_received = self.socket_udp.recvfrom(1024)
+            try:
+                #Recebendo mensagem
+                self.msg_received_udp = msg_received[0].decode('utf-8')
+                print(self.msg_received_udp)
+                #print(self.name_p2p + ": " + self.msg_received)
+                self.receive_msg_udp = self.name_p2p + ": " + self.msg_received_udp
+                print(self.receive_msg_udp)
+            except:
+                pass
+    
+    def receive_udp(self, arquivo):
+        self.text_received_udp = arquivo
+        #self.txt_chat.insert(END, current_time + self.text_received)
 
+    
     def start(self):
         self.window.mainloop()
 
@@ -174,51 +209,67 @@ class Login:
 
         #Labels de nome e porta
         self.label1 = Label(self.canva, text="Nome: ", anchor = W)
-        self.label2 = Label(self.canva, text="Porta: ", anchor = W)
+        self.label2 = Label(self.canva, text="Porta TCP: ", anchor = W)
+        #self.label4 = Label(self.canva, text="IP: ", anchor = W)
 
         #Declarando as áreas de Texto
         self.txt_name = Entry(self.canva, width = 85, border = 1, bg = 'white')
-        self.txt_port = Entry(self.canva, width = 85, border = 1, bg = 'white')   
-
+        self.txt_port_tcp = Entry(self.canva, width = 85, border = 1, bg = 'white')
+        self.txt_port_udp = Entry(self.canva, width = 85, border = 1, bg = 'white')
+        #self.txt_ip = Entry(self.canva, width = 85, border = 1, bg = 'white')   
+   
         #Declarando o Botão
         self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)        
         
         #Posicionando os Widgets
         self.label1.grid(column = 0, row = 1, columnspan = 1)
         self.label2.grid(column = 0, row = 5, columnspan = 1)
+        #self.label4.grid(column = 0, row = 7, columnspan = 1)
         self.txt_name.grid(column = 1 , row = 1, columnspan = 3)
-        self.txt_port.grid(column = 1, row = 5, columnspan = 3)
-        self.send_button.grid(column = 5, row = 1)
+        self.txt_port_tcp.grid(column = 1, row = 5, columnspan = 3)
+        #self.txt_ip.grid(column = 1, row = 7, columnspan = 3)
 
+        self.send_button.grid(column = 5, row = 1)
 
     def send(self, event = None):
         #Pegando as infromações de nome e porta e fechando a janela
         self.text_nome = self.txt_name.get()
-        self.text_porta = int(self.txt_port.get())
+        self.text_porta_tcp = int(self.txt_port_tcp.get())
         self.window.destroy()
 
     def start(self):
         self.window.mainloop()
-        return self.text_nome, self.text_porta
+        return self.text_nome, self.text_porta_tcp
 
 class Server:
-
     def __init__(self):
         #Porta do server
-        self.port = 50000
+        self.port_tcp = 50000
+        self.port_udp = 50001
 
-        #Criando socket do server
-        self.server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
-        self.server.bind(('localhost', self.port))
+        #Criando socket TCP do server
+        self.server_tcp = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+        self.server_tcp.bind(('localhost', self.port_tcp))
+
+        #Criando socket UDP do server
+        self.server_udp = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+        self.server_udp.bind(('localhost', self.port_udp))
 
         #Inicia o server
         self.start()
 
     #Aceita a conexão do cliente e recebe seus dados
     def accept_client(self):
-        client, addr = self.server.accept()
+        client, addr = self.server_tcp.accept()
         name = client.recv(1024).decode('utf-8')
-        client_list = [client, addr[0], addr[1], name]
+        print("Nome: " + name)
+
+        addr_pair = self.server_udp.recvfrom(1024)
+        print("Addr: " + str(addr_pair[1]))
+        command = addr_pair[0].decode('utf-8')
+        address = addr_pair[1]
+
+        client_list = [client, addr[0], addr[1], name, address[1]]
 
         return client_list
 
@@ -229,9 +280,12 @@ class Server:
         client_reciever[0].sendall(client_sender[1].encode('utf-8'))
         time.sleep(0.03)
         client_reciever[0].sendall(str(client_sender[2]).encode('utf-8'))
+        time.sleep(0.03)
+        client_reciever[0].sendall(str(client_sender[4]).encode('utf-8'))
+        print("Troca Concluída")
 
     def start(self):
-        self.server.listen(2)
+        self.server_tcp.listen(2)
         self.client1 = self.accept_client()
         self.client2 = self.accept_client()
 
@@ -240,9 +294,11 @@ class Server:
         self.send_data(self.client2, self.client1)
 
         #Fecha a conexão do server
-        self.server.close()
+        self.server_tcp.close()
+        self.server_udp.close()
 
-class UDP:
+
+'''class UDP:
 
     def __int__(self):
         self.port = 50000
@@ -271,10 +327,10 @@ class UDP:
             pass
 
             #se deu timeout, reenvia o pacote
-            '''if (timeout):
+            if (timeout):
                 self.sock_udt.sendto(self.pkt_emissor, self.serverAddressPort)
                 #reinicia o contador
-                self.start_time()'''
+                self.start_time()
 
         #Se o pacote tiver o ack desejado e não estiver corrompido, para o timer
         elif (msgFromReceptor.split()[1] == 'ACK' and (msgFromReceptor.split()[0] != '{self.contador}' or ~self.corrupt(msgFromReceptor))):
@@ -330,4 +386,4 @@ class UDP:
 
     def corrupt(self, pkt):
         checksum_analise = self.checksum(bytes(pkt[1]))
-        return checksum_analise != pkt[2]
+        return checksum_analise != pkt[2]'''
