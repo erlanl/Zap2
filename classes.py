a9d147f8b82ecbf4295609f08216d095ab84831b
GuilhermeMaciel75 <guilhermemacielmelo75@hotmail.com>
2022-10-29 00:37:37 -0300

    Organizing button on window

--- classes.py
+++ classes.py
@@ -44,21 +44,21 @@ class Chat:
 
     def createWidgets(self):
         #Declarando os widgets do chat
-        self.txt_chat = ScrolledText(self.canva, border = 1)
+        self.txt_chat = ScrolledText(self.canva, border = 1, state=DISABLED)
         self.txt_chat.pack()
-        self.txt_field = Entry(self.canva, width = 85, border = 1, bg = 'white')
-        self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)
-        self.clean_button = Button(self.canva, text = "Limpar", padx = 40, command = self.clean)
-        self.upload_button = Button(self.canva, text = "Arquivo", padx = 40, command = self.upload)
-        
+        self.txt_field = Entry(self.canva, width = 90, border = 1, bg = 'white')
+        self.send_button = Button(self.canva, text = "Enviar", padx = 39, command = self.send)
+        self.clean_button = Button(self.canva, text = "Limpar", padx = 38, command = self.clean)
+        self.upload_button = Button(self.canva, text = "Arquivo", padx = 35, command = self.upload)
+        self.window.bind('<Return>', self.send)
         
         #Posicionando os Widgets
         self.txt_chat.config(background = "#363636", foreground='white')
         self.txt_chat.grid(column = 0, row = 0, columnspan = 3)
-        self.txt_field.grid(column = 0, row = 1, columnspan = 2)
+        self.txt_field.grid(column = 0, row = 2, columnspan = 2)
         self.send_button.grid(column = 2, row = 1)
-        self.clean_button.grid(column = 3, row = 1)
-        self.upload_button.grid(column = 4, row = 1)
+        self.clean_button.grid(column = 2, row = 2)
+        self.upload_button.grid(column = 2, row = 3)
     
     def playSong(self):
         pygame.mixer.music.load(self.window.filename)
@@ -74,6 +74,7 @@ class Chat:
         global play_button
         global play_video_button
 
+        self.txt_chat.configure(state=NORMAL)
         #ENVIA UMA MENSAGEM UDP AO CLICAR EM UPLOAD
         #self.addr -> ENDEREÇO UDP DO OUTRO CLIENTE
         try:
@@ -110,9 +111,13 @@ class Chat:
             self.txt_chat.window_create(END, window=Button(self.window, text="Play Video", padx = 40, command=self.playVideo))
             self.txt_chat.insert(END, '\n')
 
+        self.txt_chat.configure(state=DISABLED)
+
     def clean(self):
         #Limpando tela de mensagens
+        self.txt_chat.configure(state=NORMAL)
         self.txt_chat.delete("1.0", "end")
+        self.txt_chat.configure(state=DISABLED)
 
     def send(self, event = None):
         self.text = self.txt_field.get()
@@ -121,7 +126,9 @@ class Chat:
         #Impedindo o envio de mensagens vazias
         if text_aux != "":
             current_time = "<" + datetime.now().strftime('%d/%m/%Y %H:%M') + "h" + "> "
+            self.txt_chat.configure(state=NORMAL)
             self.txt_chat.insert(END, current_time + self.name + ": " + self.text + '\n')
+            self.txt_chat.configure(state=DISABLED)
             self.txt_field.delete(0, END)
 
             #Enviando a mensagem para o outro usuário
@@ -133,7 +140,9 @@ class Chat:
     def receive(self, text):
         self.text_received = text + "\n"
         current_time = "<" + datetime.now().strftime('%d/%m/%Y %H:%M') + "h" + "> "
+        self.txt_chat.configure(state=NORMAL)
         self.txt_chat.insert(END, current_time + self.text_received)
+        self.txt_chat.configure(state=DISABLED)
 
     def connect_server(self):
         #Abrindo conexão com o server
@@ -219,7 +228,8 @@ class Login:
         #self.txt_ip = Entry(self.canva, width = 85, border = 1, bg = 'white')   
    
         #Declarando o Botão
-        self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)        
+        self.send_button = Button(self.canva, text = "Enviar", padx = 40, command = self.send)    
+        self.window.bind('<Return>', self.send)    
         
         #Posicionando os Widgets
         self.label1.grid(column = 0, row = 1, columnspan = 1)
